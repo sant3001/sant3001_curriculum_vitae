bb3e80f64ce3ecd5f3849edafe2dcc607977a1eb
Santiago Bendavid <santiago.bendavid@ocelotbot.com>
2023-08-10 17:39:27 -0500

    Add print styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import React, { FC, PropsWithChildren } from 'react';
 import './layout.css';
+import './layout.print.css';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return <div className="container-lg a4">{children}</div>;
